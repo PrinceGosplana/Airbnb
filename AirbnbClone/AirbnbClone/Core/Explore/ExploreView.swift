ad0af1272baf741968aeaf2678fff2a59e475576
Oleksandr Isaiev <oleksandr.isaiev@thetrainline.com>
2024-04-06 17:46:47 +0300

    add llisting view elements

---
+++ AirbnbClone/AirbnbClone/Core/Explore/ExploreView.swift
@@ -0,0 +1,29 @@
+//
+//  ExploreView.swift
+//  AirbnbClone
+//
+//  Created by Oleksandr Isaiev on 06.04.2024.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0...10, id: \.self) { listing in
+                        ListingItemView()
+                            .frame(height: 400)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
