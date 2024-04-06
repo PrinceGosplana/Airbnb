747faa9e466f40c4199d4ed3439f0454b884ee00
Oleksandr Isaiev <oleksandr.isaiev@thetrainline.com>
2024-04-06 09:07:36 +0300

    init project

---
+++ AirbnbClone/AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Oleksandr Isaiev on 06.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
