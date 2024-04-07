64f30b46c08d4362aa174fedfbc10d3fff0d9c7a
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-07 16:11:34 +0300

    add beedrooms section

--- AirbnbClone/AirbnbClone/Core/Listings/ListingDetailView.swift
+++ AirbnbClone/AirbnbClone/Core/Listings/ListingDetailView.swift
@@ -91,6 +91,36 @@ struct ListingDetailView: View {
                 }
             }
             .padding()
+
+            Divider()
+
+            // beedrooms view
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Where you'll sleep")
+                    .font(.headline)
+
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 16) {
+                        ForEach(1 ..< 9) { bedroom in
+                            VStack {
+                                Image(systemName: "bed.double")
+
+                                Text("Bedroom \(bedroom)")
+                            }
+                            .frame(width: 132, height: 100)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(lineWidth: 1)
+                                    .foregroundStyle(.gray)
+                            }
+                        }
+                    }
+                }
+                .scrollTargetBehavior(.paging)
+            }
+            .padding()
+
+            Divider()
         }
     }
 }
