91a12f016454e96bf47ec6d56fff2b4776aa6664
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-08 13:40:39 +0300

    add where to go field

--- AirbnbClone/AirbnbClone/Core/Explore/DestinationSearchView.swift
+++ AirbnbClone/AirbnbClone/Core/Explore/DestinationSearchView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct DestinationSearchView: View {
 
     @Binding var show: Bool
+    @State private var destination = ""
 
     var body: some View {
         VStack {
@@ -22,6 +23,32 @@ struct DestinationSearchView: View {
                     .imageScale(.large)
                     .foregroundStyle(.black)
             }
+
+            VStack(alignment: .leading) {
+                Text("Where to?")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .imageScale(.small)
+
+                    TextField("Search destinations", text: $destination)
+                        .font(.subheadline)
+                }
+                .frame(minHeight: 44)
+                .padding(.horizontal)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(lineWidth: 1.0)
+                        .foregroundStyle(Color(.systemGray4))
+                }
+            }
+            .padding()
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
         }
     }
 }
