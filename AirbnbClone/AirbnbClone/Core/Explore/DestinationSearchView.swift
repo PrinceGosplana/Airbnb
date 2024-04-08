cfeb1935dd3178770f71b9b9238780303a8cbb5b
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-08 11:02:29 +0300

    add destination search view

---
+++ AirbnbClone/AirbnbClone/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationSearchView.swift
+//  AirbnbClone
+//
+//  Created by Oleksandr Isaiev on 08.04.2024.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
